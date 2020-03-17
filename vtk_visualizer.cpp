72b7e654ad027b5834f91bc7122141593e8a76c7
Jeremy Xu <jeremyxu1998@gmail.com>
2020-03-17 00:54:50 -0400

    add disk number calculation to vtk tendon visualization

--- vtk_visualizer.cpp
+++ vtk_visualizer.cpp
@@ -138,24 +138,25 @@ bool VtkVisualizer::UpdateVisualization(std::vector<Eigen::Matrix4d> allDisksPos
     backboneFunctionSource->Update();
     backboneTubeFilter->Update();
 
+    unsigned segBaseDiskCount = 0;
     for (unsigned segCount = 0; segCount < segTendonNum.size(); segCount++) {
         for (unsigned tendonCount = 0; tendonCount < segTendonNum[segCount]; tendonCount++) {
             vtkNew<vtkPoints> tendonPoints;
-            for (int diskCount = 0; diskCount < segDiskNum[segCount]; diskCount++) {
+            for (unsigned diskCount = 0; diskCount < segDiskNum[segCount]; diskCount++) {
                 Eigen::Vector4d tendonPosRel;  // Position of the tendon hole relative to disk center
-                // tendonPosRel << segPitchRad[0], 0.0, 0.0, 1.0;
                 double radius = segPitchRad[segCount];
-                tendonPosRel << radius * cos(tendonCount * 2.0 * M_PI / 3.0), radius * sin(tendonCount * 2.0 * M_PI / 3.0), 0.0, 1.0;
-                // Calculate the disk count from base to query for pose
-                //unsigned diskTotalCount = 0;
-                //for (unsigned i = 0; segCount < segTendonNum.size(); segCount++)
-                Eigen::Vector4d tendonPosWorld = allDisksPose[segCount * 8 + diskCount] * tendonPosRel;
+                tendonPosRel << radius * cos(tendonCount * 2.0 * M_PI / static_cast<double>(segTendonNum[segCount])),
+                                radius * sin(tendonCount * 2.0 * M_PI / static_cast<double>(segTendonNum[segCount])),
+                                0.0,
+                                1.0;
+                Eigen::Vector4d tendonPosWorld = allDisksPose[segBaseDiskCount + diskCount] * tendonPosRel;
                 double point[3] = {tendonPosWorld(0), tendonPosWorld(1), tendonPosWorld(2)};
                 tendonPoints->InsertNextPoint(point);
             }
             tendonLines[segCount][tendonCount]->SetPoints(tendonPoints);
             tendonTubeFilters[segCount][tendonCount]->Update();
         }
+        segBaseDiskCount += segDiskNum[segCount];
     }
 
     renderWindow->Render();
@@ -170,28 +171,13 @@ bool VtkVisualizer::SetDiskPose(vtkSmartPointer<vtkActor> actor, const Eigen::Ma
                      0.0, 1.0, 0.0, 0.0,
                      0.0, 0.0, 0.0, 1.0;  // Rotate disk around its x-axis for pi/2
     Eigen::Matrix4d displayPose = pose * vtkDisplayMat;
-    vtkSmartPointer<vtkMatrix4x4> vtkPose = vtkSmartPointer<vtkMatrix4x4>::New();
-    vtkPose->SetElement(0,0,displayPose(0,0));  // TODO
-    vtkPose->SetElement(0,1,displayPose(0,1));
-    vtkPose->SetElement(0,2,displayPose(0,2));
-    vtkPose->SetElement(0,3,displayPose(0,3));
-    vtkPose->SetElement(1,0,displayPose(1,0));
-    vtkPose->SetElement(1,1,displayPose(1,1));
-    vtkPose->SetElement(1,2,displayPose(1,2));
-    vtkPose->SetElement(1,3,displayPose(1,3));
-    vtkPose->SetElement(2,0,displayPose(2,0));
-    vtkPose->SetElement(2,1,displayPose(2,1));
-    vtkPose->SetElement(2,2,displayPose(2,2));
-    vtkPose->SetElement(2,3,displayPose(2,3));
-    vtkPose->SetElement(3,0,displayPose(3,0));
-    vtkPose->SetElement(3,1,displayPose(3,1));
-    vtkPose->SetElement(3,2,displayPose(3,2));
-    vtkPose->SetElement(3,3,displayPose(3,3));
-
-//    const double *test = displayPose.transpose().data();
-//    const double test2[16] = {test[0],test[1],test[2],test[3],test[4],test[5],test[6],test[7],test[8],test[9],test[10],test[11],test[12],test[13],test[14],test[15]};
-////    std::cout << test[1] << std::endl;
-//    vtkPose->DeepCopy(test2);
+    vtkNew<vtkMatrix4x4> vtkPose;
+    // vtkMatrix4x4::DeepCopy() not working as expected, copy element by element instead
+    for (int row = 0; row < 4; row++) {
+        for (int col = 0; col < 4; col++) {
+            vtkPose->SetElement(row, col, displayPose(row,col));
+        }
+    }
     actor->SetUserMatrix(vtkPose);
 
     return true;
